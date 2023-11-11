badc5ff9e8cd374df53179a9ba3d778f0dc45715
Daniel <daniilterawars@gmail.com>
2023-11-11 18:48:19 +0300

    feat(filter): remove facets

--- src/modules/realty-object/realty-object.controller.ts
+++ src/modules/realty-object/realty-object.controller.ts
@@ -67,33 +67,6 @@ export class RealtyObjectController {
     return this.realtyObjectService.getSpecs();
   }
 
-  @Get('/facets')
-  @UsePipes(
-    new ValidationPipe({
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  )
-  getFacets(@Query() queryParams: FilterQueryParamsDto) {
-    return this.realtyObjectService.getFacets({
-      limit: queryParams.limit,
-      offset: queryParams.offset,
-      minPrice: queryParams.min_price,
-      maxPrice: queryParams.max_price,
-      minArea: queryParams.min_area,
-      maxArea: queryParams.max_area,
-      minFloor: queryParams.min_floor,
-      maxFloor: queryParams.max_floor,
-      benefits: queryParams.benefits?.split(',').map((item) => +item) ?? [],
-      furnish: queryParams.furnish,
-      property: queryParams.property,
-      rooms: queryParams.rooms?.split(',').map((item) => +item) ?? [],
-      type: queryParams.type?.split(',').map((item) => item.trim()) ?? [],
-      sort: queryParams.sort,
-      order: queryParams.order,
-    });
-  }
-
   @Get(':id')
   findOneById(@Param('id') id: string) {
     return this.realtyObjectService.findOneById(+id);
